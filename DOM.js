bf8754cec6a8821e864ae63398d1733994c3d430
sanjucs12 <sanjucs12@gmail.com>
2023-03-08 17:35:01 +0530

    first commit

---
+++ DOM.js
@@ -0,0 +1,16 @@
+// console.dir(document)
+// console.log(document.title)
+// document.title=12345;
+// console.log(document.title)
+// console.log(document.body);
+// console.log(document.all[10]);
+var headertitle=document.getElementById("header-title")
+console.log(headertitle.innerText);
+console.log(headertitle.textContent);
+
+// headertitle.textContent="Hello Items";
+//console.log(headertitle.textContent);
+
+
+
+
